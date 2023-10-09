e0decfede90a3a6349827a5da18348587cf85a9b
CorboPy <alex.corbett48@gmail.com>
2023-10-09 19:57:56 +0100

    Analysis for threshold scans

--- udaq/tools/analyse.py
+++ udaq/tools/analyse.py
@@ -2,11 +2,43 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import mplhep as hep
+import os
 hep.style.use('LHCb2')
 
-df = pd.read_csv('C:/Users/kp14102/CR_MSci_Lab/Run0011.csv')
+os.system("python ./udaq/src/udaq.py ./udaq/tools/udaq_single.config")
 
-for ch in ['A','B','C','D','E','F','G','H']:
+df = pd.read_csv('C:/Users/xz21360/CR code/CR_MSci_Lab/Run0000.csv')
+
+
+time = 600 #in seconds
+N=250
+#array of thresholds to compare to
+thresholds = np.linspace(0,1.5,N)
+
+
+#filter thresholds from thresholds array
+lengths  = np.zeros(N)
+for i in range(0,N):    
+    df_filter = df.query(f"pulse_height_A>{thresholds[i]}")
+    lengths[i] = len(df_filter)
+
+plt.errorbar(thresholds,lengths/time,yerr=np.sqrt(lengths)/time)
+plt.xlabel('Threshold (V)',fontsize=32)
+#plt.yscale("log")  
+plt.ylabel('Trigger Rate',fontsize=32)
+plt.savefig('rate_thresh.pdf')
+plt.clf()
+plt.cla()
+plt.errorbar(thresholds,lengths/time,yerr=np.sqrt(lengths)/time)
+plt.xlabel('Threshold (V)',fontsize=32)
+plt.yscale("log")  
+plt.ylabel('Trigger Rate',fontsize=32)
+plt.savefig('rate_thresh_log.pdf')
+
+
+
+
+""" for ch in ['A','B','C','D','E','F','G','H']:
     h, bins = np.histogram(df[f'pulse_height_{ch}'],range=[0,1],bins=100)
     hep.histplot(h,bins,yerr=np.sqrt(h),histtype='step',label=f'Channel {ch}')
 
@@ -24,7 +56,8 @@ thF = 0.1
 thG = 0.1
 thH = 0.01
 
-
+# run for trigger 1 save events/time , run for trigger 2 .......
+# run for long then , plot for pulse hight over x 
 df_BDF = df.query(f"pulse_height_B>{thB} & pulse_height_D>{thD} & pulse_height_F>{thF} & pulse_height_H<{thH}")
 df_BDH = df.query(f"pulse_height_B>{thB} & pulse_height_D>{thD} & pulse_height_H>{thH} & pulse_height_F<{thF}")
 df_BFH = df.query(f"pulse_height_B>{thB} & pulse_height_F>{thF} & pulse_height_H>{thH} & pulse_height_D<{thD}")
@@ -36,4 +69,4 @@ print(f"Number of BDH triggers and !F: {df_BDH.shape[0]}")
 print(f"Number of BFH triggers and !D: {df_BFH.shape[0]}")
 print(f"Number of DFH triggers and !B: {df_DFH.shape[0]}")
 print(f"Number of BDFH triggers: {df_BDFH.shape[0]}")
-print(f"Total number of triggers: {df.shape[0]}")
+print(f"Total number of triggers: {df.shape[0]}") """
