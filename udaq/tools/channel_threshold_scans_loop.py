be8aa95c440b36994be042114ea908ad163841eb
CorboPy <alex.corbett48@gmail.com>
2024-01-28 23:02:38 +0000

    jan 2024

--- udaq/tools/channel_threshold_scans_loop.py
+++ udaq/tools/channel_threshold_scans_loop.py
@@ -8,12 +8,21 @@ hep.style.use('LHCb2')
 #os.system("python ./udaq/src/udaq.py ./udaq/tools/udaq per channel.config")
 #doesn't work - need to use anaconda powershell to run experiment and use this file to analyse it
 
-channels = ["A","B","C","E","F","G"]    #missing out D and H as they have range 0.1 and range 0.5 datasets 
+channels = ["A","B","C","D","E","F","G","H"]    #missing out D and H as they have range 0.1 and range 0.5 datasets 
 
+threshold_y = [0,0,0,0,0,0,0,0]
+threshold_x = [0.1200, 0.0900, 0.1400, 0.0500, 0.0800, 0.0400, 0.0700, 0.0300]
+colour_array = [0,0,0,0,0,0,0,0]
+fs = "35" # Font size
+
+
+th_i = -1
 for channel in channels:
-
-    df = pd.read_csv('C:/Users/alexc/Documents/#Uni/Physics with Astrophysics/YR 3/YR3 Labs/CR Experiment/threshold_analysis/Channel_%s/Run0000.csv' % channel)    #change dir
-
+    th_i += 1
+    if (channel == "D" or channel == "H"):
+        df = pd.read_csv('C:/Users/alexc/Documents/#Uni/Physics with Astrophysics/YR 3/YR3 Labs/CR Experiment/threshold_analysis/Channel_%s/Run0000_range0.5.csv' % channel)    #change dir
+    else:
+        df = pd.read_csv('C:/Users/alexc/Documents/#Uni/Physics with Astrophysics/YR 3/YR3 Labs/CR Experiment/threshold_analysis/Channel_%s/Run0000.csv' % channel)    #change dir
 
     time = 600 #in seconds
     N=250
@@ -25,15 +34,80 @@ for channel in channels:
     for i in range(0,N):    
         df_filter = df.query(f"pulse_height_%s>{thresholds[i]}" % channel)                  
         lengths[i] = len(df_filter)
+    
+    yvals = lengths/time
+    yerrs = 1*np.sqrt(lengths/time)     #95% confidence - poisson estimate of error sqrt(lambda/time) (lambda is the mean number of events within a given interval of time or space)
+    
 
-    plt.errorbar(thresholds,lengths/time,yerr=np.sqrt(lengths)/time)
-    plt.xlabel('Threshold (V)',fontsize=32)
+    plot = plt.errorbar(thresholds,yvals, yerr = yerrs, linewidth = 2, label= ("Channel %s" % channel),elinewidth=1,capthick=1) #add:   ,marker='.',markersize=5)    for markers
+    plt.xlabel('Threshold (V)',fontsize=fs)
     #plt.yscale("log")  
-    plt.ylabel('Trigger Rate (Hz)',fontsize=32)
-    plt.xlim(0, 0.3)
-    plt.savefig('rate_thresh_%s_0000.pdf' % channel)                            
-    plt.clf()
-    plt.cla()
+    plt.ylabel('Trigger Rate (Hz)',fontsize=fs)
+    plt.xlim(0, 0.2)
+    plt.ylim(-15,300)
+
+    xthresh = threshold_x[th_i]
+    difference_array = np.absolute((thresholds)-xthresh)
+    # find the index of minimum element from the array
+    index = difference_array.argmin()
+    yval = yvals[index]
+    threshold_y[th_i] = yval
+    #print(threshold_y)
+
+    colour_array[th_i] = plot[0].get_color()
+    print(colour_array)
+
+#correcting positions of threshold markers (some are too low/high)
+base_C = threshold_y[2]
+threshold_y[2] = base_C -0.2
+
+base_D = threshold_y[3]
+threshold_y[3] = base_D -0.3
+
+base_E = threshold_y[4]
+threshold_y[4] = base_E -0.5
+
+base_F = threshold_y[5]
+threshold_y[5] = base_F +0.7
+
+base_G = threshold_y[6]
+threshold_y[6] = base_G +0.7
+
+# #x corrections
+
+# xbase_A = threshold_x[0]
+# threshold_x[0] = xbase_A + 0.0008
+
+# xbase_B = threshold_x[1]
+# threshold_x[1] = xbase_B + 0.00075
+
+# xbase_C = threshold_x[2]
+# threshold_x[2] = xbase_C -0.0009
+
+# xbase_D = threshold_x[3]
+# threshold_x[3] = xbase_D - 0.0015
+
+# xbase_E = threshold_x[4]
+# threshold_x[4] = xbase_E - 0.0015
+
+# xbase_F = threshold_x[5]
+# threshold_x[5] = xbase_F + 0.0025
+
+# xbase_G = threshold_x[6]
+# threshold_x[6] = xbase_G + 0.0025
+
+
+plt.scatter(threshold_x,threshold_y,s=250,marker="x",zorder=10,c=colour_array)
+plt.xticks(fontsize=fs)
+plt.yticks(fontsize=fs)
+# plt.grid(linestyle='--',which='both')
+# ax = plt.gca()
+# ax.minorticks_on()
+plt.legend(fontsize=fs)
+plt.show()
+#plt.savefig('thresholds.pdf')                            
+    # plt.clf()
+    # plt.cla()
     # plt.errorbar(thresholds,lengths/time,yerr=np.sqrt(lengths)/time)
     # plt.xlabel('Threshold (V)',fontsize=32)
     # plt.yscale("log")  
---
+++ udaq/tools/monte_carlo_loop.py
@@ -0,0 +1,71 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+import mplhep as hep
+hep.style.use('LHCb2')
+
+# Number of random points to sample
+N = 100
+N_2D = N**4
+
+# Initialize variables
+theta_max = np.pi/2 - np.arctan(0.22/(np.sqrt(0.409878**2 + 0.409878**2)))      # Trig calc for theta max assuming square scintillators of same area
+h=0.22 #m
+
+xvals = np.linspace(-0.40/2,0.40/2,N)     #0.409878 metres approximating a square with same area
+yvals = np.linspace(-0.420/2,0.420/2,N) 
+theta_vals = np.zeros(N)
+phi_vals = np.zeros(N)
+
+# Setting up arrays of theta and phi values
+for j in range(len(phi_vals)):
+    phi_vals[j] = np.random.uniform(0,2*np.pi)  # Azimuthal angle (φ)
+for i in range(len(theta_vals)):
+    theta_vals[i] = np.random.uniform(0,theta_max)  # Polar angle (θ)
+
+alphas = np.zeros(10)
+for r in range(len(alphas)):
+    
+    count = 0
+    index = 0
+    # Perform Monte Carlo simulation
+    for x in xvals:
+        index=index+1
+        print(round(index/N * 100,1), f"% complete")
+        for y in yvals:
+
+        # Check x and y projections onto scintillator plate at distance h both lie within the bounds of the square
+            
+            for theta in theta_vals:
+                for phi in phi_vals:
+                    
+                    if (-0.40/2 <= ((h*np.tan(theta)*np.sin(phi))+x) <= 0.40/2) and (-0.420/2 <= (((h*np.tan(theta)*np.cos(phi)))+y) <= 0.420/2):
+                        count=count+1
+
+    print("Successes:", count,". Failuires:",N_2D-count)    # N_2D instead of N because count goes like N**4
+    print("alpha=", count/N_2D, "pm", np.sqrt(((count/N_2D) * (1 - (count/N_2D)) )/ N_2D))
+    alphas[r] = count/N_2D
+
+print(alphas)
+# # Plotting example setting vector origin to (0,0) on lower scintillator plate
+# x_arr = xvals + (h*np.tan(theta_vals)*np.sin(phi_vals))
+# y_arr = yvals + (h*np.tan(theta_vals)*np.cos(phi_vals))
+
+# font = {'fontname':'Calibri'}
+# aperture = patch.Rectangle((-0.409878,-0.409878),2*0.409878,2*0.409878,fill=False,color='red')
+
+# plt.scatter(x_arr,y_arr,marker="x",color='black',label="vector trajectory")
+# plt.xlabel("$x (m)$")
+# plt.ylabel("$y (m)$")
+# plt.legend(loc="upper right",frameon=True)
+# plt.text(-0.4,0.36,"upper scintillator target",fontsize="xx-large",color='red',weight='bold',**font)
+# plt.title("Example Monte Carlo method for N=%s vectors with origin (0,0) on lower scintillator" % N,fontsize="x-large",weight='bold')
+# plt.locator_params(axis='both', nbins=5) 
+
+# ax = plt.gca()
+# ax.add_patch(aperture)
+# ax.set_xlim(-0.7,0.7)
+# ax.set_ylim(-0.7,0.7)
+
+# plt.show()
